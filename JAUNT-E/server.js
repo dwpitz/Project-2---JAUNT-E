b8af39afa0ebdae4ad497a24b6b5d07ff4fe04ab
David Pitz <dw_pitz@yahoo.com>
2019-10-30 10:13:11 -0500

    work on google maps branch

--- JAUNT-E/server.js
+++ JAUNT-E/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const secretInfo = require('./secretinfo.js')
+const googleMapsClient = require('@google/maps')
 
 require('./db/db')
 
