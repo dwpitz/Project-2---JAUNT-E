d02d0b1d51a141301a0705eed05396358cbb4117
David Pitz <dw_pitz@yahoo.com>
2019-10-30 16:16:17 -0500

    working on poi delete route

--- JAUNT-E/controllers/poi.js
+++ JAUNT-E/controllers/poi.js
@@ -92,16 +92,36 @@ router.put('/:id', async (req, res, next) => {
 })
 
 //poi delete route
+//we need to delete the poi that's a subdocument of the jaunt
 router.delete('/:id', async (req, res, next) => {
 	try {
-		const foundJaunt = await Jaunt.find({})
-		const deletePoi = await Poi.deleteOne({_id: req.params.id})
-		jauntId.poi.push(createdPoi)
-		jauntId.save((err) => {
-			if(err) next(err)
-		})
-		console.log(deletePoi);
-		res.redirect('/poi')
+		//finds all of the Jaunts...
+		console.log(req.params, ' is req params');
+		const foundPoi = await Poi.find(req.params._id)
+		const removePoi = await Jaunt.poi.id(req.params._id).remove()
+		console.log(removePoi);
+
+		
+		
+
+
+
+		//find the jaunt with a specific poi ID.
+		// const foundPoi = await Poi.find({foundJaunt, _id: '5db8594e60f4639dcc7feb90'})
+		// console.log(foundPoi);
+		//remove the poi ID from the jaunt
+
+
+		// console.log(Poi.find({foundJaunt, _id: '5db8594e60f4639dcc7feb90'}), 'the id of POI');
+		// const deletePoi = await Poi.deleteOne({_id: req.params.id})
+		// console.log(deletePoi, 'deletePoi');
+		// jauntId.poi.push(createdPoi)
+		// console.log(createdPoi, 'createdPoi')
+		// jauntId.save((err) => {
+		// 	if(err) next(err)
+		// })
+		// console.log(deletePoi);
+		// res.redirect('/poi')
 	}
 	catch (err) {
 		next(err)
